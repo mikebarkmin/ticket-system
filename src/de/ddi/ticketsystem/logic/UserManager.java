f11a9d02aa4b16d07bbc37207601541977adf319
Gero Behler <gero.behler@gmail.com>
2016-07-17 12:12:29 +0200

    javadoc

--- src/de/ddi/ticketsystem/logic/UserManager.java
+++ src/de/ddi/ticketsystem/logic/UserManager.java
@@ -7,17 +7,28 @@ public class UserManager {
     private List<User> users;
     private User current;
 
+    /**
+     * Erstellt ein Objekt der Klasse UserManager, die alle Nutzer in einer Liste verwaltet
+     */
     public UserManager() {
         users = new List<>();
         current = null;
     }
 
-    public void add(User... users) {
-        for(int i = 0; i < users.length; i++) {
-            this.users.add(users[i]);
-        }
+    /**
+     * Fügt einen Nutzer zur Verwaltung durch den UserManager hinzu.
+     * @param user Der Nutzer, der durch den UserManager verwaltet werden soll
+     */
+    public void add(User user) {
+        users.add(user);
     }
 
+    /**
+     * Wählt im UserManager einen Nutzer anhand des Vor- und Nachnamens als aktuellen Nutzer aus. Existiert
+     * kein Nutzer mit dem angegebenen Namen, passiert nichts
+     * @param firstName Der Vorname des Nutzers
+     * @param lastName Der Nachname des Nutzers
+     */
     public void login(String firstName, String lastName) {
         for(int i = 0; i < users.size(); i++) {
             User user = users.get(i);
@@ -27,6 +38,9 @@ public class UserManager {
         }
     }
 
+    /**
+     * Logt den aktuellen Nutzer aus. Ist kein Nutzer eingelogt, passiert nichts.
+     */
     public void logout() {
         current = null;
     }
@@ -35,10 +49,18 @@ public class UserManager {
         return current;
     }
 
+    /**
+     * Gibt eine Liste aller Nutzer zurück.
+     * @return List<User> mit allen Nutzern, die der UserManager verwaltet
+     */
     public List<User> getAll() {
         return users;
     }
 
+    /**
+     * Geht die Liste der Nutzer durch und gibt die Liste der Kunden zurück.
+     * @return List<Customer> mit den Kunden, die der UserManager verwaltet
+     */
     public List<Customer> getCustomers() {
         List<Customer> customers = new List<>();
         for(int i = 0; i < users.size(); i++) {
@@ -49,7 +71,11 @@ public class UserManager {
         }
         return customers;
     }
-    
+
+    /**
+     * Geht die Liste der Nutzer durch und gibt die Liste der Angestellten zurück.
+     * @return List<Employee> mit den Angestellten, die der UserManager verwaltet
+     */
     public List<Employee> getEmployees() {
         List<Employee> employees = new List<>();
         for(int i = 0; i < users.size(); i++) {

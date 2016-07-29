38ca26a69309cee0d6c38399501c616e54d710d3
Mike Barkmin <mbarkmin@gmail.com>
2016-07-29 11:08:08 +0200

    comments

--- src/de/ddi/ticketsystem/Ticket.java
+++ src/de/ddi/ticketsystem/Ticket.java
@@ -1,10 +1,26 @@
 package de.ddi.ticketsystem;
 
 public class Ticket {
+    /**
+     * Beschreibung des Tickets
+     */
     private String description;
+    /**
+     * Bearbeitungsstatus kann sein RECORDED (Aufgenommen), PROCESSED (Bearbeitet), WAITING_FOR_FEEDBACK (Auf Rückmeldung
+     * warten), SOLVED (Erledigt) und CLOSED (abgeschlossen)
+     */
     private String status;
+    /**
+     * Name des Angestellten, der das Ticket bearbeiten soll
+     */
     private String employee;
+    /**
+     * Name des Kunden, der das Ticket in Auftrag gegeben hat
+     */
     private String customer;
+    /**
+     * Priorität des Tickets
+     */
     private int priority;
 
     /**
@@ -23,43 +39,75 @@ public class Ticket {
         this.priority = priority;
     }
 
+    /**
+     * @return gibt die Beschreibung des Tickets zurück
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * @param description Beschreibung des Tickets
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * @return gibt den Status des Tickets zurück
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * @param status Status des Tickets
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * @return gibt den Namen des zugewiesenen Angestellten
+     */
     public String getEmployee() {
         return employee;
     }
 
+    /**
+     * @param employee Name des Angestellten, welcher dem Ticket zugewiesen wird
+     */
     public void setEmployee(String employee) {
         this.employee = employee;
     }
 
+    /**
+     * @return gibt den Namen des Kunden zurück für den das Ticket bearbeitet wird
+     */
     public String getCustomer() {
         return customer;
     }
 
+    /**
+     * @param customer Name des Kunden, für den das Ticket bearbeitet wird
+     */
     public void setCustomer(String customer) {
         this.customer = customer;
     }
 
+    /**
+     * @return gibt die Priorität des Tickets zurück
+     */
     public int getPriority() {
         return priority;
     }
 
+    /**
+     * Es werden nur Prioritäten größer als 0 erlaubt. Wenn ein Wert kleiner 0 gesetzt werden soll, passiert nichts.
+     * @param priority Priorität des Tickets
+     */
     public void setPriority(int priority) {
+        // Überprüfung, ob die Priorität größer als 0 ist
         if (priority > 0) {
             this.priority = priority;
         }

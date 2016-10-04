311c495db1339f4e690aea6388e611c6f5c13ab8
Gero02 <gero.behler@gmail.com>
2016-10-04 13:33:19 +0200

    New Stage. main function with control structures to add new tickets

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,80 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Ticket ticket = new Ticket(
+
+        // Initialisierungen
+        Ticket[] tickets = new Ticket[100];
+        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter(System.lineSeparator());
+        String input;
+
+        // Zwei Beispieltickets
+        tickets[count] = new Ticket(
                 "Beschreibung",
                 "In Bearbeitung",
                 "Mike Barkmin",
                 "Paluno",
                 1);
-        
-        System.out.println(ticket.getCustomer());
+        count++;
 
-        // Der Default-Konstuktor exisitert nur, wenn kein anderer deklariert ist oder
-        // wenn er expliziert deklariert wurde.
-        //Ticket ticket2 = new Ticket();
+        tickets[count] = new Ticket(
+                "Beschreibung",
+                "In Bearbeitung",
+                "Gero Behler",
+                "Paluno",
+                1);
+        count++;
+
+        // Schleife zur Nutzerinteraktion
+        do {
+            System.out.println("[T]ickets, [H]inzufügen, [B]eenden");
+            input = scanner.next();
+            input = input.toUpperCase();
+
+            switch(input) {
+                case "T":
+                    for(Ticket ticket : tickets)
+                    {
+                        if (ticket != null) {
+                            if(ticket != null) {
+                                System.out.println(ticket.getStatus() + "\t " + ticket.getDescription() + "\t "
+                                        + ticket.getPriority() + "\t " + ticket.getEmployee() + "\t"
+                                        + ticket.getCustomer());
+                            }
+                        }
+                    }
+                    break;
+
+                case "H":
+                    // Dateneingabe zum Ticket
+                    System.out.print("Beschreibung: ");
+                    String description = scanner.next();
+                    System.out.print("Status: ");
+                    String status = scanner.next();
+                    System.out.print("Priorität: ");
+                    int priority = scanner.nextInt();
+                    System.out.println("Angestellter: ");
+                    String employee = scanner.next();
+                    System.out.println("Kunde: ");
+                    String customer = scanner.next();
+
+                    // Ticket zum Array hinzufügen und Zähler inkrementieren
+                    Ticket toAdd = new Ticket(description, status, employee, customer, priority);
+                    tickets[count] = toAdd;
+                    count++;
+                    break;
+
+                case "B":
+                    System.out.println("Programm wird beendet.");
+                    break;
+
+                default:
+                    System.out.println("Ungültige Eingabe.");
+                    break;
+            }
+
+        } while(!input.equals("B"));
     }
 }

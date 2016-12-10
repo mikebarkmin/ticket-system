cc4aa51819693a4736e9c9f93b23b14565c3c18b
Mike Barkmin <mbarkmin@gmail.com>
2016-12-10 09:48:58 +0100

    remove find

--- src/de/ddi/ticketsystem/presentation/TicketsView.java
+++ src/de/ddi/ticketsystem/presentation/TicketsView.java
@@ -29,8 +29,7 @@ public class TicketsView extends View {
 
         options = new String[]{
                 "[A]uswählen",
-                "[Ä]ltestes",
-                "[F]inden"
+                "[Ä]ltestes"
         };
         employeeOptions = new String[]{
                 "[N]ächstes",
@@ -115,22 +114,9 @@ public class TicketsView extends View {
                 break;
             case "S":
                 sort();
-            case "F":
-                searchTickets();
-                break;
             default:
                 break;
         }
-    }
-
-    /**
-     * Methode zum Suchen nach einem Ticket
-     */
-    private void searchTickets() {
-        System.out.println("Suchbegriff: ");
-        String search = scanner.next();
-        List<Ticket> filteredTickets = tickets.search(search);
-        viewManager.setNextView(new TicketsSearchView(viewManager, filteredTickets));
     }
 
     /**

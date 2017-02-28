cdab091c465feab361e1a86b3bb9ed88e5153842
Mike Barkmin <mbarkmin@gmail.com>
2017-02-28 16:56:33 +0100

    added tests

---
+++ tests/src/RequestTicketTest.java
@@ -0,0 +1,110 @@
+import de.ddi.ticketsystem.Customer;
+import de.ddi.ticketsystem.Employee;
+import de.ddi.ticketsystem.RequestTicket;
+import de.ddi.ticketsystem.Status;
+import de.uni_due.s3.jack2.backend.checkers.tracingchecker.framework.TracingFramework;
+import de.uni_due.s3.jack2.backend.checkers.tracingchecker.framework.TracingFramework.Test;
+
+public class RequestTicketTest {
+	int punkte = 0;
+	
+	public int getResult() {
+		return punkte;
+	}
+	
+	@Test(name="constructor")
+	public void constructor() {
+		String description = "des";
+		Status status = Status.CLOSED;
+		Employee employee = new Employee(null, null, null, null);
+		Customer customer = new Customer(null, null, null, null);
+		int priority = 1;
+        String service = "dev";
+        Date date = new Date();
+		RequestTicket t = new RequestTicket(description, status, employee, customer, priority, date, service);
+		if (StaticMethods.getValueFromField(t, "description") == description) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Das Attribut description wird falsch gesetzt.");
+		}
+		if (StaticMethods.getValueFromField(t, "status") == status) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Das Attribut status wird falsch gesetzt.");
+		}
+		if (StaticMethods.getValueFromField(t, "employee") == employee) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Das Attribut employee wird falsch gesetzt.");
+		}
+		if (StaticMethods.getValueFromField(t, "customer") == customer) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Das Attribut customer wird falsch gesetzt.");
+		}
+		if (StaticMethods.getValueFromField(t, "service") == service) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Das Attribut service wird falsch gesetzt.");
+		}
+		if (StaticMethods.getValueFromField(t, "date") == date) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Das Attribut date wird falsch gesetzt.");
+		}
+	}
+	
+	@Test(name="getter")
+	public void getter() {
+		String description = "des";
+		Status status = Status.CLOSED;
+		Employee employee = new Employee(null, null, null, null);
+		Customer customer = new Customer(null, null, null, null);
+		int priority = 1;
+        String service = "dev";
+        Date date = new Date();
+		RequestTicket t = new RequestTicket(description, status, employee, customer, priority, date, service);
+		StaticMethods.setValueToField(t, "description", description);
+		StaticMethods.setValueToField(t, "status", status);
+		StaticMethods.setValueToField(t, "employee", employee);
+		StaticMethods.setValueToField(t, "customer", customer);
+		StaticMethods.setValueToField(t, "priority", priority);
+		StaticMethods.setValueToField(t, "service", service);
+		StaticMethods.setValueToField(t, "date", date);
+		if (t.getDescription() == description) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Die Methode getDescription gibt ein falschen Wert zurück.");
+		}
+		if (t.getStatus() == status) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Die Methode getStatus gibt ein falschen Wert zurück.");
+		}
+		if (t.getEmployee() == employee) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Die Methode getEmployee gibt ein falschen Wert zurück.");
+		}
+		if (t.getCustomer() == customer) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Die Methode getCustomer gibt ein falschen Wert zurück.");
+		}
+		if (t.getPriority() == priority) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Die Methode getPriority gibt ein falschen Wert zurück.");
+		}
+		if (t.getService()) == service) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Die Methode getService gibt ein falschen Wert zurück.");
+		}
+		if (t.getDate()) == date) {
+			punkte += 1;
+		} else {
+			TracingFramework.printError("Die Methode getDate gibt ein falschen Wert zurück.");
+		}
+	}
+}
---
+++ tests/src/StaticMethods.java
@@ -0,0 +1,40 @@
+import java.lang.reflect.Field;
+import de.uni_due.s3.jack2.backend.checkers.tracingchecker.framework.TracingFramework;
+
+public class StaticMethods {
+    public static Object getValueFromField(Object o, String name) {
+        Field field;
+        try {
+            field = o.getClass().getDeclaredField(name);
+            field.setAccessible(true);
+            Object value = field.get(o);
+            return value;
+        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+        	try {
+            	field = o.getClass().getSuperclass().getDeclaredField(name);
+                field.setAccessible(true);
+                field.set(o, content);
+            } catch (Exception b) {
+            	TracingFramework.printError("Das Attribut " + name + " wurde nicht gefunden");
+            }
+        }
+        return null;
+    }
+
+    public static void setValueToField(Object o, String name, Object content) {
+        Field field;
+        try {
+            field = o.getClass().getDeclaredField(name);
+            field.setAccessible(true);
+            field.set(o, content);
+        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+            try {
+            	field = o.getClass().getSuperclass().getDeclaredField(name);
+                field.setAccessible(true);
+                field.set(o, content);
+            } catch (Exception b) {
+            	TracingFramework.printError("Das Attribut " + name + " wurde nicht gefunden");
+            }
+        }
+    }
+}

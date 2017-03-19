9de9421976674d0557a316954cf16833bf4cc6ab
Gero02 <gero.behler@gmail.com>
2017-03-19 18:35:07 +0100

    removed interface saveable

--- src/de/ddi/ticketsystem/logic/Saveable.java
+++
@@ -1,8 +0,0 @@
-package de.ddi.ticketsystem.logic;
-
-/**
- * Interface, welches dazu dient eine allgemeine Schnittstelle für das Speichern von Objekten zu schaffen.
- */
-public interface Saveable {
-    String saveToText();
-}

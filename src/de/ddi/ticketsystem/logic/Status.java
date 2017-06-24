15684d794382af0226de223e0018875073f610e8
Gero02 <gero.behler@gmail.com>
2017-06-24 12:49:48 +0200

    Updated scaffold to match the version used in the video.

--- src/de/ddi/ticketsystem/logic/Status.java
+++
@@ -1,8 +0,0 @@
-package de.ddi.ticketsystem.logic;
-
-/**
- * Die Enumeration Status definiert die unterschiedlichen Status die ein Ticket annehmen kann.
- */
-public enum Status {
-    RECORDED, PROCESSED, WAITING_FOR_FEEDBACK, SOLVED, CLOSED
-}
